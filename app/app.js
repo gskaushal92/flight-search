3d81ee5e43dbe1195d9375fb2b2d3766a42a6875
Gopal Sharma <gsharma027@nam.ad.pwcinternal.com>
2017-07-14 11:59:42 +0530

    get json data and displya

--- app/app.js
+++ app/app.js
@@ -6,6 +6,25 @@ angular.module('myApp',[
 config(['$routeProvider',function($routeProvider){
 	$routeProvider.otherwise({redirectTo: '/'});
 }])
-.controller('FlightSearchCtrl', ['$scope',function($scope) {
-	$scope.flight="Test flight";
-}]);
+.controller('FlightSearchCtrl', ['$scope','$http','$filter',function($scope,$http,$filter) {
+
+	$scope.flights={};
+	$http.get("data/data.json").then(function(response){
+		$scope.flights=response.data;
+		console.log($scope.flights);
+	});
+
+
+	$scope.searchFlights=function(){
+
+		$scope.flightDetails=$filter('filter')($scope.flights.flights, {'source':'PNQ'}|| {'destination':'DEL'});
+		console.log($scope.flightDetails);
+	 }
+
+	// FlightService.GetFlights().then(function(data){
+	// 	console.log(data);
+	// });
+	
+	// $scope.flight="Test flight";
+}])
+;
